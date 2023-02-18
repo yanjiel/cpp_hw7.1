3fcb089ff122d1eb0072a4c876f5d9d2fb91373a
yanjiel <yanjieliu829@gmail.com>
2023-02-18 00:18:30 -0600

    commit

--- hw7.1.cpp
+++ hw7.1.cpp
@@ -10,28 +10,50 @@
 
 #include<iostream>
 #include<future>
-#include<chrono>
+#include<type_traits>
 
 using namespace std;
 using namespace std::chrono_literals;
 
-template<typename Func, typename T, typename ...Args>
+
+//template<typename Func, typename T, typename ...Args>
 //auto my_async (Func f, Args && ... args ) { 
 //	packaged_task<Func> pt{ f };
 //	future<T> fv = pt.get_future();
 //	pt(std::forward<Args>(args)...);
 //}
 
+template<typename Func, typename ...Args>
 auto my_async(Func f, Args ... args) {
-	packaged_task<Func> pt{ f };
-	future<T> fv = pt.get_future();
-	pt(args...);
+	
+	packaged_task<decltype(f(args...))(Args...)> pt(f);
+	future<decltype(f(args...))> fv = pt.get_future();
+
+	pt(args..);
+	//std::jthread jt(std::move(pt), args...);
+	return fv;
 }
 
 
-auto sleep = []() {this_thread::sleep_for(5s); };
+int counter(int i = 0) {
+	for (auto cnt = 0; cnt < i; cnt++) { 
+		cout << cnt; 
+	}
+	cout << "done counting" << std::endl;  
+	return i; 
+};
 
 int main() {
-	return 0;
 
+	std::future<int> f1 = async(counter,0);
+	std::future<int> f2 = async(counter,0);
+
+	//std::future<int> f3 = my_async(counter);
+	//std::future<int> f4 = my_async(counter);
+
+	cout << f1.get()<<std::endl;
+	cout << f2.get()<<std::endl;
+	/*cout << f3.get() << std::endl;
+	cout << f4.get() << std::endl;*/
+	return 0;
 }
